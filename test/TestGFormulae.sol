e71d6f4712b0e41760ecd44dfb11e5f8c6cf1360
etsvigun <utgarda@gmail.com>
2020-10-15 18:23:33 +0100

    testing _calcDepositSharesFromCost with (deposit fee != cost)

--- test/TestGFormulae.sol
+++ test/TestGFormulae.sol
@@ -9,6 +9,13 @@ import { GFormulae } from "../contracts/GFormulae.sol";
 
 contract TestGFormulae is Env
 {
+	/* deposit(cost):
+	 *   price = reserve / supply = 1
+	 *   gross = cost / price     = 100e18
+	 *   net = gross * 0.99       =  99e18
+	 *   fee = gross - net        =   1e18
+	 *   return net, fee
+	 */
 	function test01() public
 	{
 		uint256 _cost1 = 100e18;
@@ -17,6 +24,66 @@ contract TestGFormulae is Env
 		uint256 _depositFee = 1e16;
 		(uint256 _netShares, uint256 _feeShares1) = GFormulae._calcDepositSharesFromCost(_cost1, _totalReserve, _totalSupply, _depositFee);
 		Assert.equal(_netShares, 99e18, "net shares must be 99e18");
+		(uint256 _cost2, uint256 _feeShares2) = GFormulae._calcDepositCostFromShares(_netShares, _totalReserve, _totalSupply, _depositFee);
+		Assert.equal(_cost1, _cost2, "costs must be equal");
+		Assert.equal(_feeShares1, _feeShares2, "fee shares must be equal");
+	}
+
+	/* deposit(cost):
+	 *   price = reserve / supply = 1
+	 *   gross = cost / price     = 10e18
+	 *   net = gross * 0.99       = 99e17
+	 *   fee = gross - net        =  1e17
+	 *   return net, fee
+	 */
+	function test011() public
+	{
+		uint256 _cost1 = 10e18;
+		uint256 _totalReserve = 100e18;
+		uint256 _totalSupply = 100e18;
+		uint256 _depositFee = 1e16;
+		(uint256 _netShares, uint256 _feeShares1) = GFormulae._calcDepositSharesFromCost(_cost1, _totalReserve, _totalSupply, _depositFee);
+		Assert.equal(_netShares, 99e17, "net shares must be 99e17");
+		(uint256 _cost2, uint256 _feeShares2) = GFormulae._calcDepositCostFromShares(_netShares, _totalReserve, _totalSupply, _depositFee);
+		Assert.equal(_cost1, _cost2, "costs must be equal");
+		Assert.equal(_feeShares1, _feeShares2, "fee shares must be equal");
+	}
+
+	/* deposit(cost):
+	 *   price = reserve / supply = 1
+	 *   gross = cost / price     = 100e18
+	 *   net = gross * 0.99       =  99e18
+	 *   fee = gross - net        =   1e18
+	 *   return net, fee
+	 */
+	function test012() public
+	{
+		uint256 _cost1 = 100e18;
+		uint256 _totalReserve = 1000e18;
+		uint256 _totalSupply = 1000e18;
+		uint256 _depositFee = 1e15;
+		(uint256 _netShares, uint256 _feeShares1) = GFormulae._calcDepositSharesFromCost(_cost1, _totalReserve, _totalSupply, _depositFee);
+		Assert.equal(_netShares, 99e18, "net shares must be 99e18");
+		(uint256 _cost2, uint256 _feeShares2) = GFormulae._calcDepositCostFromShares(_netShares, _totalReserve, _totalSupply, _depositFee);
+		Assert.equal(_cost1, _cost2, "costs must be equal");
+		Assert.equal(_feeShares1, _feeShares2, "fee shares must be equal");
+	}
+
+	/* deposit(cost):
+	 *   price = reserve / supply = 1
+	 *   gross = cost / price     = 10e18
+	 *   net = gross * 0.99       = 99e17
+	 *   fee = gross - net        =  1e17
+	 *   return net, fee
+	 */
+	function test013() public
+	{
+		uint256 _cost1 = 10e18;
+		uint256 _totalReserve = 100e18;
+		uint256 _totalSupply = 100e18;
+		uint256 _depositFee = 1e16; //back to _depositFee == _cost1 / 1000 , this test passes
+		(uint256 _netShares, uint256 _feeShares1) = GFormulae._calcDepositSharesFromCost(_cost1, _totalReserve, _totalSupply, _depositFee);
+		Assert.equal(_netShares, 99e17, "net shares must be 99e17");
 		(uint256 _cost2, uint256 _feeShares2) = GFormulae._calcDepositCostFromShares(_netShares, _totalReserve, _totalSupply, _depositFee);
 		Assert.equal(_cost1, _cost2, "costs must be equal");
 		Assert.equal(_feeShares1, _feeShares2, "fee shares must be equal");
