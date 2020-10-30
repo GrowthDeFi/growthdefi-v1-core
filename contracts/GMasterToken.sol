bf05359773a63d6acb1b7959292ba64485ff40eb
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2020-10-30 18:09:42 -0300

    Finalizes the bulk of the rebalancing algorithm

--- contracts/GMasterToken.sol
+++ contracts/GMasterToken.sol
@@ -8,26 +8,18 @@ import { GTokenBase } from "./GTokenBase.sol";
 import { GCToken } from "./GCToken.sol";
 import { G } from "./G.sol";
 
-// missing 0.1% fee / month collected at each operation
-// streaming fee accumulated and then claimed to an external contract
-// which by default is the locked LP
-// replacing the external contract applies immediately but requires
-// liquidity migration policy of 7-days
-
 contract GMasterToken is GTokenBase
 {
 	using SafeMath for uint256;
 	using EnumerableSet for EnumerableSet.AddressSet;
 
 	EnumerableSet.AddressSet private tokens;
-	mapping (address => uint256) private weights;
-	uint256 totalWeight;
+	mapping (address => uint256) private percents;
 
 	constructor (string memory _name, string memory _symbol, uint8 _decimals, address _stakesToken, address _reserveToken)
 		GTokenBase(_name, _symbol, _decimals, _stakesToken, _reserveToken) public
 	{
-		weights[_reserveToken] = 1e18;
-		totalWeight = 1e18;
+		percents[_reserveToken] = 1e18;
 	}
 
 	function tokenCount() public view returns (uint256 _tokenCount)
@@ -42,19 +34,16 @@ contract GMasterToken is GTokenBase
 
 	function tokenPercent(address _token) public view returns (uint256 _percent)
 	{
-		return weights[_token].mul(1e18).div(totalWeight);
+		return percents[_token];
 	}
 
-	function setTokenPercent(address _token, uint256 _percent) public onlyOwner nonReentrant
+	function transferTokenPercent(address _sourceToken, address _targetToken, uint256 _percent) public onlyOwner nonReentrant
 	{
-		require(_percent < 1e18, "Invalid percent");
-		if (_token != reserveToken) require(tokens.contains(_token), "Unknown token");
-		uint256 _weight = weights[_token];
-		totalWeight = totalWeight.sub(_weight);
-		uint256 _factor = _percent.mul(1e18).div(uint256(1e18).sub(_percent));
-		_weight = totalWeight.mul(_factor).div(1e18);
-		weights[_token] = _weight;
-		totalWeight = totalWeight.add(_weight);
+		require(_percent <= percents[_sourceToken], "Invalid percent");
+		require(_sourceToken != _targetToken, "Invalid transfer");
+		require(_targetToken == reserveToken || tokens.contains(_targetToken), "Unknown token");
+		percents[_sourceToken] -= _percent;
+		percents[_targetToken] += _percent;
 	}
 
 	function insertToken(address _token) public onlyOwner nonReentrant
@@ -66,7 +55,8 @@ contract GMasterToken is GTokenBase
 
 	function removeToken(address _token) public onlyOwner nonReentrant
 	{
-		require(weights[_token] == 0, "Positive percent");
+		require(percents[_token] == 0, "Positive percent");
+		require(G.getBalance(_token) == 0, "Unbalanced reserve");
 		require(tokens.remove(_token), "Unknown token");
 	}
 
@@ -82,52 +72,102 @@ contract GMasterToken is GTokenBase
 		return _totalReserve;
 	}
 
-	function _prepareDeposit(uint256 _cost) internal override returns (bool _success) {
+	function _prepareDeposit(uint256 _cost) internal override returns (bool _success)
+	{
 		return _adjustReserve(_cost);
 	}
 
-	function _prepareWithdrawal(uint256 _cost) internal override returns (bool _success) {
+	function _prepareWithdrawal(uint256 _cost) internal override returns (bool _success)
+	{
 		return _adjustReserve(_cost);
 	}
 
-	function _adjustReserve(uint256 _roomAmount) internal returns (bool _success) {
-		uint256 _reserveAmount = totalReserve();
-		_roomAmount = G.min(_roomAmount, _reserveAmount);
-		_reserveAmount = _reserveAmount.sub(_roomAmount);
-		uint256 _tokenCount = tokenCount();
-		for (uint256 _i = 0; _i < _tokenCount; _i++) {
-			address _token = tokenAt(_i);
-			uint256 _tokenPercent = tokenPercent(_token);
-			uint256 _newTokenReserve = _reserveAmount.mul(_tokenPercent).div(1e18);
-			uint256 _oldTokenReserve = GCToken(_token).totalReserveUnderlying();
-			if (_newTokenReserve < _oldTokenReserve) {
-				uint256 _amount = _oldTokenReserve.sub(_newTokenReserve);
-				uint256 _grossShares = _calcSharesFromUnderlyingCost(_token, _amount);
-				if (_grossShares > 0) {
-					try GCToken(_token).withdrawUnderlying(_grossShares) {
-					} catch (bytes memory /* _data */) {
-						return false;
-					}
-				}
+	function _adjustReserve(uint256 _roomAmount) internal returns (bool _success)
+	{
+		(uint256 _which, address _token, uint256 _underlyingCostOrGrossShares) = _findAdjustReserveOperation(_roomAmount);
+		if (_which == 1) {
+			uint256 _underlyingCost = _underlyingCostOrGrossShares;
+			G.approveFunds(reserveToken, _token, _underlyingCost);
+			try GCToken(_token).depositUnderlying(_underlyingCost) {
+				return true;
+			} catch (bytes memory /* _data */) {
+				G.approveFunds(reserveToken, _token, 0);
+				return false;
 			}
 		}
-		for (uint256 _i = 0; _i < _tokenCount; _i++) {
-			address _token = tokenAt(_i);
-			uint256 _tokenPercent = tokenPercent(_token);
-			uint256 _newTokenReserve = _reserveAmount.mul(_tokenPercent).div(1e18);
-			uint256 _oldTokenReserve = GCToken(_token).totalReserveUnderlying();
-			if (_newTokenReserve > _oldTokenReserve) {
-				uint256 _amount = _newTokenReserve.sub(_oldTokenReserve);
-				try GCToken(_token).depositUnderlying(_amount) {
-				} catch (bytes memory /* _data */) {
-					return false;
-				}
+		if (_which == 2) {
+			uint256 _grossShares = _underlyingCostOrGrossShares;
+			try GCToken(_token).withdrawUnderlying(_grossShares) {
+				return true;
+			} catch (bytes memory /* _data */) {
+				return false;
 			}
 		}
 		return true;
 	}
 
-	function _calcSharesFromUnderlyingCost(address _token, uint256 _underlyingCost) internal view returns (uint256 _grossShares) {
+	function _findAdjustReserveOperation(uint256 _roomAmount) internal view returns (uint256 _which, address _adjustToken, uint256 _underlyingCostOrGrossShares)
+	{
+		_which = 0;
+		uint256 _maxPercent = 0; // add minimum
+
+		uint256 _reserveAmount = totalReserve();
+		_roomAmount = G.min(_roomAmount, _reserveAmount);
+		_reserveAmount = _reserveAmount.sub(_roomAmount);
+
+		uint256 _tokenCount = tokenCount();
+		for (uint256 _i = 0; _i < _tokenCount; _i++) {
+			address _token = tokenAt(_i);
+
+			uint256 _oldTokenReserve = GCToken(_token).totalReserveUnderlying();
+			uint256 _oldTokenPercent = _oldTokenReserve.mul(1e18).div(_reserveAmount);
+
+			uint256 _idealTokenReserve;
+			{
+				uint256 _tokenPercent = percents[_token];
+				_idealTokenReserve = _reserveAmount.mul(_tokenPercent).div(1e18);
+			}
+
+			if (_idealTokenReserve > _oldTokenReserve) {
+				uint256 _underlyingCost = _idealTokenReserve.sub(_oldTokenReserve);
+				_underlyingCost = G.min(_underlyingCost, G.getBalance(reserveToken));
+
+				uint256 _newTokenReserve = _oldTokenReserve.add(_underlyingCost);
+				uint256 _newTokenPercent = _newTokenReserve.mul(1e18).div(_reserveAmount);
+
+				uint256 _percent = _newTokenPercent.sub(_oldTokenPercent);
+				if (_percent > _maxPercent) {
+					_maxPercent = _percent;
+					_which = 1;
+					_adjustToken = _token;
+					_underlyingCostOrGrossShares = _underlyingCost;
+				}
+				continue;
+			}
+			if (_idealTokenReserve < _oldTokenReserve) {
+				uint256 _underlyingCost = _oldTokenReserve.sub(_idealTokenReserve);
+				uint256 _grossShares = _calcWithdrawalSharesFromUnderlyingCost(_token, _underlyingCost);
+				_grossShares = G.min(_grossShares, G.getBalance(_token));
+				_underlyingCost = _calcWithdrawalUnderlyingCostFromShares(_token, _grossShares);
+
+				uint256 _newTokenReserve = _oldTokenReserve.sub(_underlyingCost);
+				uint256 _newTokenPercent = _newTokenReserve.mul(1e18).div(_reserveAmount);
+
+				uint256 _percent = _newTokenPercent.sub(_oldTokenPercent);
+				if (_percent > _maxPercent) {
+					_maxPercent = _percent;
+					_which = 2;
+					_adjustToken = _token;
+					_underlyingCostOrGrossShares = _grossShares;
+				}
+				continue;
+			}
+		}
+		return (_which, _adjustToken, _underlyingCostOrGrossShares);
+	}
+
+	function _calcWithdrawalSharesFromUnderlyingCost(address _token, uint256 _underlyingCost) internal view returns (uint256 _grossShares)
+	{
 		uint256 _totalReserve = GCToken(_token).totalReserve();
 		uint256 _totalSupply = GCToken(_token).totalSupply();
 		uint256 _withdrawalFee = GCToken(_token).withdrawalFee();
@@ -136,26 +176,13 @@ contract GMasterToken is GTokenBase
 		return _grossShares;
 	}
 
-	// streamming fee
-
-	uint256 constant STREAMING_FEE_PER_SECOND = 385609697; // ((1 + 0.001) ** (1 / (30 * 24 * 60 * 60))) - 1
-
-	function _calcStreamingFee(uint256 _totalSupply, uint256 _lastTime, uint256 _thisTime) internal pure returns (uint256 _shares)
+	function _calcWithdrawalUnderlyingCostFromShares(address _token, uint256 _grossShares) internal view returns (uint256 _underlyingCost)
 	{
-		uint256 _ellapsed = _thisTime.sub(_lastTime);
-		uint256 _factor = _powpi(STREAMING_FEE_PER_SECOND.add(1e18), _ellapsed).sub(1e18);
-		_shares = _totalSupply.mul(_factor).div(1e18);
-		return _shares;
-	}
-
-	function _powpi(uint256 _base, uint256 _exp) internal pure returns (uint256 _power)
-	{
-		_power = 1e18;
-		while (_exp > 0) {
-			if (_exp & 1 != 0) _power = _power.mul(_base).div(1e18);
-			_base = _base.mul(_base).div(1e18);
-			_exp >>= 1;
-		}
-		return _power;
+		uint256 _totalReserve = GCToken(_token).totalReserve();
+		uint256 _totalSupply = GCToken(_token).totalSupply();
+		uint256 _withdrawalFee = GCToken(_token).withdrawalFee();
+		uint256 _exchangeRate = GCToken(_token).exchangeRate();
+		(_underlyingCost,) = GCToken(_token).calcWithdrawalUnderlyingCostFromShares(_grossShares, _totalReserve, _totalSupply, _withdrawalFee, _exchangeRate);
+		return _underlyingCost;
 	}
 }
