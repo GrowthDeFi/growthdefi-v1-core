ac963d07f16ce847a3e6001e1ea5701d506215e3
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2020-11-02 18:03:05 -0300

    Reviews GPortfolioReserveManager

--- contracts/GPortfolioReserveManager.sol
+++ contracts/GPortfolioReserveManager.sol
@@ -70,7 +70,7 @@ library GPortfolioReserveManager
 
 	function setRebalanceMargin(Self storage _self, uint256 _rebalanceMargin) public
 	{
-		require(0 < _rebalanceMargin && _rebalanceMargin < 1e18, "Invalid margin");
+		require(0 <= _rebalanceMargin && _rebalanceMargin <= 1e18, "Invalid margin");
 		_self.rebalanceMargin = _rebalanceMargin;
 	}
 
@@ -85,27 +85,32 @@ library GPortfolioReserveManager
 		_roomAmount = G.min(_roomAmount, _reserveAmount);
 		_reserveAmount = _reserveAmount.sub(_roomAmount);
 
-		{
-			uint256 _liquidAmount = G.getBalance(_self.reserveToken);
-			uint256 _requiredAmount = _roomAmount.sub(G.min(_liquidAmount, _roomAmount));
-			if (_requiredAmount == 0) {
-				uint256 _tokenPercent = _self.percents[_self.reserveToken];
-				uint256 _newTokenReserve = _liquidAmount.sub(_roomAmount);
-				uint256 _newTokenPercent = _newTokenReserve.mul(1e18).div(_reserveAmount);
-				if (_newTokenPercent > _tokenPercent) {
-					uint256 _percent = _newTokenPercent.sub(_tokenPercent);
-					if (_percent < _self.rebalanceMargin) return true;
-				}
-				else
-				if (_newTokenPercent < _tokenPercent) {
-					uint256 _percent = _tokenPercent.sub(_newTokenPercent);
-					if (_percent < _self.rebalanceMargin) return true;
-				}
-			}
+		uint256 _liquidAmount = G.getBalance(_self.reserveToken);
+		uint256 _blockedAmount = G.min(_roomAmount, _liquidAmount);
+		_liquidAmount = _liquidAmount.sub(_blockedAmount);
+
+		uint256 _requiredAmount = _roomAmount.sub(_blockedAmount);
+		if (_requiredAmount > 0) {
+			(address _adjustToken, uint256 _adjustAmount) = _self._findRequiredWithdrawal(_reserveAmount, _requiredAmount);
+			if (_adjustToken == address(0)) return false;
+			return _self._withdrawUnderlying(_adjustToken, _adjustAmount);
 		}
 
-		(uint256 _which, address _token, uint256 _underlyingCostOrGrossShares) = _self._findAdjustReserveOperation(_reserveAmount);
-		return _self._executeOperation(_which, _token, _underlyingCostOrGrossShares);
+		(uint256 _depositAmount, uint256 _withdrawalAmount) = _self._calcLiquidAdjustment(_reserveAmount, _liquidAmount);
+
+		if (_depositAmount > 0) {
+			(address _adjustToken, uint256 _adjustAmount) = _self._findDeposit(_reserveAmount);
+			if (_adjustToken == address(0)) return true;
+			return _self._depositUnderlying(_adjustToken, G.min(_adjustAmount, _depositAmount));
+		}
+
+		if (_withdrawalAmount > 0) {
+			(address _adjustToken, uint256 _adjustAmount) = _self._findWithdrawal(_reserveAmount);
+			if (_adjustToken == address(0)) return true;
+			return _self._withdrawUnderlying(_adjustToken, G.min(_adjustAmount, _withdrawalAmount));
+		}
+
+		return true;
 	}
 
 	function _calcTotalReserve(Self storage _self) internal view returns (uint256 _totalReserve)
@@ -120,36 +125,60 @@ library GPortfolioReserveManager
 		return _totalReserve;
 	}
 
-	function _executeOperation(Self storage _self, uint256 _which, address _token, uint256 _underlyingCostOrGrossShares) internal returns (bool _success)
+	function _calcLiquidAdjustment(Self storage _self, uint256 _reserveAmount, uint256 _liquidAmount) internal view returns (uint256 _depositAmount, uint256 _withdrawalAmount)
 	{
-		if (_which == 0) {
-			return true;
+		uint256 _tokenPercent = _self.percents[_self.reserveToken];
+		uint256 _tokenReserve = _reserveAmount.mul(_tokenPercent).div(1e18);
+		if (_liquidAmount > _tokenReserve) {
+			uint256 _upperPercent = G.min(1e18, _tokenPercent.add(_self.rebalanceMargin));
+			uint256 _upperReserve = _reserveAmount.mul(_upperPercent).div(1e18);
+			if (_liquidAmount > _upperReserve) return (_liquidAmount.sub(_tokenReserve), 0);
 		}
-		if (_which == 1) {
-			uint256 _underlyingCost = _underlyingCostOrGrossShares;
-			G.approveFunds(_self.reserveToken, _token, _underlyingCost);
-			try GCToken(_token).depositUnderlying(_underlyingCost) {
-				return true;
-			} catch (bytes memory /* _data */) {
-				G.approveFunds(_self.reserveToken, _token, 0);
-				return false;
-			}
+		else
+		if (_liquidAmount < _tokenReserve) {
+			uint256 _lowerPercent = _tokenPercent.sub(G.min(_tokenPercent, _self.rebalanceMargin));
+			uint256 _lowerReserve = _reserveAmount.mul(_lowerPercent).div(1e18);
+			if (_liquidAmount < _lowerReserve) return (0, _tokenReserve.sub(_liquidAmount));
 		}
-		if (_which == 2) {
-			uint256 _grossShares = _underlyingCostOrGrossShares;
-			try GCToken(_token).withdrawUnderlying(_grossShares) {
-				return true;
-			} catch (bytes memory /* _data */) {
-				return false;
-			}
-		}
-		assert(false);
+		return (0, 0);
 	}
 
-	function _findAdjustReserveOperation(Self storage _self, uint256 _reserveAmount) internal view returns (uint256 _which, address _adjustToken, uint256 _underlyingCostOrGrossShares)
+	function _findRequiredWithdrawal(Self storage _self, uint256 _reserveAmount, uint256 _requiredAmount) internal view returns (address _adjustToken, uint256 _adjustAmount)
 	{
-		_which = 0;
+		uint256 _minPercent = 1e18;
+		_adjustToken = address(0);
+		_adjustAmount = 0;
+
+		uint256 _tokenCount = _self.tokens.length();
+		for (uint256 _index = 0; _index < _tokenCount; _index++) {
+			address _token = _self.tokens.at(_index);
+			uint256 _tokenReserve = GCToken(_token).totalReserveUnderlying();
+			if (_tokenReserve < _requiredAmount) continue;
+
+			uint256 _oldTokenReserve = _tokenReserve.sub(_requiredAmount);
+			uint256 _oldTokenPercent = _oldTokenReserve.mul(1e18).div(_reserveAmount);
+			uint256 _newTokenPercent = _self.percents[_token];
+
+			uint256 _percent = 0;
+			if (_newTokenPercent > _oldTokenPercent) _percent = _newTokenPercent.sub(_oldTokenPercent);
+			else
+			if (_newTokenPercent < _oldTokenPercent) _percent = _oldTokenPercent.sub(_newTokenPercent);
+
+			if (_percent < _minPercent) {
+				_minPercent = _percent;
+				_adjustToken = _token;
+				_adjustAmount = _requiredAmount;
+			}
+		}
+
+		return (_adjustToken, _adjustAmount);
+	}
+
+	function _findDeposit(Self storage _self, uint256 _reserveAmount) internal view returns (address _adjustToken, uint256 _adjustAmount)
+	{
 		uint256 _maxPercent = _self.rebalanceMargin;
+		_adjustToken = address(0);
+		_adjustAmount = 0;
 
 		uint256 _tokenCount = _self.tokens.length();
 		for (uint256 _index = 0; _index < _tokenCount; _index++) {
@@ -157,56 +186,77 @@ library GPortfolioReserveManager
 
 			uint256 _oldTokenReserve = GCToken(_token).totalReserveUnderlying();
 			uint256 _oldTokenPercent = _oldTokenReserve.mul(1e18).div(_reserveAmount);
+			uint256 _newTokenPercent = _self.percents[_token];
 
-			uint256 _idealTokenReserve;
-			{
-				uint256 _tokenPercent = _self.percents[_token];
-				_idealTokenReserve = _reserveAmount.mul(_tokenPercent).div(1e18);
+			if (_newTokenPercent > _oldTokenPercent) {
+				uint256 _percent = _newTokenPercent.sub(_oldTokenPercent);
+				if (_percent > _maxPercent) {
+					uint256 _newTokenReserve = _reserveAmount.mul(_newTokenPercent).div(1e18);
+					uint256 _amount = _newTokenReserve.sub(_oldTokenReserve);
+
+					_maxPercent = _percent;
+					_adjustToken = _token;
+					_adjustAmount = _amount;
+				}
 			}
-
-			if (_idealTokenReserve > _oldTokenReserve) {
-				uint256 _underlyingCost = _idealTokenReserve.sub(_oldTokenReserve);
-				_underlyingCost = G.min(_underlyingCost, G.getBalance(_self.reserveToken));
-
-				uint256 _percent;
-				{
-					uint256 _newTokenReserve = _oldTokenReserve.add(_underlyingCost);
-					uint256 _newTokenPercent = _newTokenReserve.mul(1e18).div(_reserveAmount);
-					_percent = _newTokenPercent.sub(_oldTokenPercent);
-				}
-
-				if (_percent > _maxPercent) {
-					_maxPercent = _percent;
-					_which = 1;
-					_adjustToken = _token;
-					_underlyingCostOrGrossShares = _underlyingCost;
-				}
-				continue;
-			}/*
-			if (_idealTokenReserve < _oldTokenReserve) {
-				uint256 _underlyingCost = _oldTokenReserve.sub(_idealTokenReserve);
-				uint256 _grossShares = _self._calcWithdrawalSharesFromUnderlyingCost(_token, _underlyingCost);
-				_grossShares = G.min(_grossShares, G.getBalance(_token));
-				_underlyingCost = _self._calcWithdrawalUnderlyingCostFromShares(_token, _grossShares);
-
-				uint256 _percent;
-				{
-					uint256 _newTokenReserve = _oldTokenReserve.sub(_underlyingCost);
-					uint256 _newTokenPercent = _newTokenReserve.mul(1e18).div(_reserveAmount);
-					_percent = _newTokenPercent.sub(_oldTokenPercent);
-				}
-
-				if (_percent > _maxPercent) {
-					_maxPercent = _percent;
-					_which = 2;
-					_adjustToken = _token;
-					_underlyingCostOrGrossShares = _grossShares;
-				}
-				continue;
-			}*/
 		}
 
-		return (_which, _adjustToken, _underlyingCostOrGrossShares);
+		return (_adjustToken, _adjustAmount);
+	}
+
+	function _findWithdrawal(Self storage _self, uint256 _reserveAmount) internal view returns (address _adjustToken, uint256 _adjustAmount)
+	{
+		uint256 _maxPercent = _self.rebalanceMargin;
+		_adjustToken = address(0);
+		_adjustAmount = 0;
+
+		uint256 _tokenCount = _self.tokens.length();
+		for (uint256 _index = 0; _index < _tokenCount; _index++) {
+			address _token = _self.tokens.at(_index);
+
+			uint256 _oldTokenReserve = GCToken(_token).totalReserveUnderlying();
+			uint256 _oldTokenPercent = _oldTokenReserve.mul(1e18).div(_reserveAmount);
+			uint256 _newTokenPercent = _self.percents[_token];
+
+			if (_newTokenPercent < _oldTokenPercent) {
+				uint256 _percent = _oldTokenPercent.sub(_newTokenPercent);
+				if (_percent > _maxPercent) {
+					uint256 _newTokenReserve = _reserveAmount.mul(_newTokenPercent).div(1e18);
+					uint256 _amount = _oldTokenReserve.sub(_newTokenReserve);
+
+					_maxPercent = _percent;
+					_adjustToken = _token;
+					_adjustAmount = _amount;
+				}
+			}
+		}
+
+		return (_adjustToken, _adjustAmount);
+	}
+
+	function _depositUnderlying(Self storage _self, address _token, uint256 _amount) internal returns (bool _success)
+	{
+		_amount = G.min(_amount, G.getBalance(_self.reserveToken));
+		if (_amount == 0) return true;
+		G.approveFunds(_self.reserveToken, _token, _amount);
+		try GCToken(_token).depositUnderlying(_amount) {
+			return true;
+		} catch (bytes memory /* _data */) {
+			G.approveFunds(_self.reserveToken, _token, 0);
+			return false;
+		}
+	}
+
+	function _withdrawUnderlying(Self storage _self, address _token, uint256 _amount) internal returns (bool _success)
+	{
+		uint256 _grossShares = _self._calcWithdrawalSharesFromUnderlyingCost(_token, _amount);
+		_grossShares = G.min(_grossShares, G.getBalance(_token));
+		if (_grossShares == 0) return true;
+		try GCToken(_token).withdrawUnderlying(_grossShares) {
+			return true;
+		} catch (bytes memory /* _data */) {
+			return false;
+		}
 	}
 
 	function _calcWithdrawalSharesFromUnderlyingCost(Self storage /* _self */, address _token, uint256 _underlyingCost) internal view returns (uint256 _grossShares)
@@ -217,15 +267,5 @@ library GPortfolioReserveManager
 		uint256 _exchangeRate = GCToken(_token).exchangeRate();
 		(_grossShares,) = GCToken(_token).calcWithdrawalSharesFromUnderlyingCost(_underlyingCost, _totalReserve, _totalSupply, _withdrawalFee, _exchangeRate);
 		return _grossShares;
-	}
-
-	function _calcWithdrawalUnderlyingCostFromShares(Self storage /* _self */, address _token, uint256 _grossShares) internal view returns (uint256 _underlyingCost)
-	{
-		uint256 _totalReserve = GCToken(_token).totalReserve();
-		uint256 _totalSupply = GCToken(_token).totalSupply();
-		uint256 _withdrawalFee = GCToken(_token).withdrawalFee();
-		uint256 _exchangeRate = GCToken(_token).exchangeRate();
-		(_underlyingCost,) = GCToken(_token).calcWithdrawalUnderlyingCostFromShares(_grossShares, _totalReserve, _totalSupply, _withdrawalFee, _exchangeRate);
-		return _underlyingCost;
 	}
 }
