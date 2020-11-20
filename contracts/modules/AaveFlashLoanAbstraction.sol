f9da9dede271449c57ad2451ec069d209a846a3c
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2020-11-20 20:17:10 -0300

    Adds comments to Aave/Dydx flash loan abstractions

--- contracts/modules/AaveFlashLoanAbstraction.sol
+++ contracts/modules/AaveFlashLoanAbstraction.sol
@@ -9,23 +9,49 @@ import { LendingPool, LendingPoolCore } from "../interop/Aave.sol";
 
 import { $ } from "../network/$.sol";
 
+/**
+ * @dev This library abstracts the Aave flash loan functionality. It has a
+ *      standardized flash loan interface. See GFlashBorrower.sol,
+ *      FlashLoans.sol, and DydxFlashLoanAbstraction.sol for further documentations.
+ */
 library AaveFlashLoanAbstraction
 {
 	using SafeMath for uint256;
 
 	uint256 constant FLASH_LOAN_FEE_RATIO = 9e14; // 0.09%
 
-	function _estimateFlashLoanFee(address /* _token */, uint256 _netAmount) internal pure returns (uint256 _feeAmount)
+	/**
+	 * @dev Estimates the flash loan fee given the reserve token and required amount.
+	 * @param _token The ERC-20 token to flash borrow from.
+	 * @param _netAmount The amount to be borrowed without considering repay fees.
+	 * @param _feeAmount the expected fee to be payed in excees of the loan amount.
+	 */
+	function _estimateFlashLoanFee(address _token, uint256 _netAmount) internal pure returns (uint256 _feeAmount)
 	{
+		_token; // silences warnings
 		return _netAmount.mul(FLASH_LOAN_FEE_RATIO).div(1e18);
 	}
 
+	/**
+	 * @dev Retrieves the current market liquidity for a given reserve.
+	 * @param _token The reserve token to flash borrow from.
+	 * @return _liquidityAmount The reserve token available market liquidity.
+	 */
 	function _getFlashLoanLiquidity(address _token) internal view returns (uint256 _liquidityAmount)
 	{
 		address _core = $.Aave_AAVE_LENDING_POOL_CORE;
 		return LendingPoolCore(_core).getReserveAvailableLiquidity(_token);
 	}
 
+	/**
+	 * @dev Triggers a flash loan. The current contract will receive a call
+	 *      back with the loan amount and should repay it, including fees,
+	 *      before returning. See GFlashBorrow.sol.
+	 * @param _token The reserve token to flash borrow from.
+	 * @param _netAmount The amount to be borrowed without considering repay fees.
+	 * @param _context Additional data to be passed to the call back.
+	 * @return _success A boolean indicating whether or not the operation suceeded.
+         */
 	function _requestFlashLoan(address _token, uint256 _netAmount, bytes memory _context) internal returns (bool _success)
 	{
 		address _pool = $.Aave_AAVE_LENDING_POOL;
@@ -36,6 +62,12 @@ library AaveFlashLoanAbstraction
 		}
 	}
 
+	/**
+	 * @dev This function should be called as the final step of the flash
+	 *      loan to properly implement the repay of the loan.
+	 * @param _token The reserve token.
+	 * @param _grossAmount The amount to be repayed including repay fees.
+	 */
 	function _paybackFlashLoan(address _token, uint256 _grossAmount) internal
 	{
 		address _poolCore = $.Aave_AAVE_LENDING_POOL_CORE;
