a69306b45bd3c4ff54dca3c0d35938713fe80d06
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2020-12-21 17:56:38 -0300

    Correction of the rAAVE migration script

--- migrations/19_deploy_GTokenType4.js
+++ migrations/19_deploy_GTokenType4.js
@@ -3,17 +3,17 @@ const names = [
 ];
 
 const G = artifacts.require('G');
-const GTokenRegistry = artifacts.require('GElasticTokenManager');
-const GTokenRegistry = artifacts.require('GPriceOracle');
+const GElasticTokenManager = artifacts.require('GElasticTokenManager');
+const GPriceOracle = artifacts.require('GPriceOracle');
 const GTokenRegistry = artifacts.require('GTokenRegistry');
 
 module.exports = async (deployer, network) => {
   const registry = await GTokenRegistry.deployed();
   for (const name of names) {
     const GToken = artifacts.require(name);
+    deployer.link(G, GToken);
     deployer.link(GElasticTokenManager, GToken);
     deployer.link(GPriceOracle, GToken);
-    deployer.link(G, GToken);
     await deployer.deploy(GToken, `${200e18}`);
     const token = await GToken.deployed();
     await registry.registerNewToken(token.address, '0x0000000000000000000000000000000000000000');
